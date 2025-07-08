3dbaa3b8c6c5a0b0bec9945d62c375f7e2638b36
HRYKKG <hiro1224koga@gmail.com>
2025-07-08 12:47:57 +0900

    Add files via upload

--- App/server_msa.R
+++ App/server_msa.R
@@ -1,10 +1,7 @@
 library(Biostrings)
 library(ggmsa)
 library(ggtree)
-#library(msa)
-
-fasta_dir=paste0(data_f,"Orthogroup_Sequences")
-tree_dir=paste0(data_f,"Gene_Trees")
+library(msa)
 
 #search functions
 seq_dataInput <- reactive({
@@ -36,12 +33,18 @@ output$seq_result_n <- renderText({
 ## get Multifasta: just outputting the fasta
 observeEvent(input$seq_get_fa, {
   if(length(seq_dataInput()) == 1){
-    file <- paste0(fasta_dir,"/",isolate(seq_dataInput()),".fa")
-    out <- paste0(readLines(file, encoding = "UTF-8"), collapse = "\n")
-  }
-  else{
+    query <- paste0("SELECT fasta FROM orthogroup_data WHERE og_id = '", isolate(seq_dataInput()), "'")
+    res <- dbGetQuery(con, query)
+    
+    if(nrow(res) == 1){
+      out <- res$fasta[1]
+    } else {
+      out <- "ERROR: No sequence found in database."
+    }
+  } else {
     out <- "ERROR: Please specify a single ortholog group."
   }
+  
   updateTextInput(session, "seq_mfasta", value = out)
 })
 
@@ -57,39 +60,69 @@ output$clip <- renderUI({
 
 ## get ggMSA
 observeEvent(input$mfasta_align, {
-  #file <- paste0(fasta_dir,"/",isolate(seq_dataInput()),".fa")
-  file <- "/tmp/seq_tmp.fa"
-  write(input$seq_mfasta, file, append = FALSE)
-  mfa <- readAAStringSet(file, "fasta")
-  msa.seqs <- msa(mfa, "ClustalOmega")
-  output$msa_plot <- renderPlot(
-    width=50+max(width(msa.seqs@unmasked))*22,
-    height=100+length(msa.seqs@unmasked)*30,
-    res = 120,
-    {
-    ggmsa(msa.seqs@unmasked, char_width = 0.5, seq_name = T) + theme(axis.text = element_text(size=12)) + geom_seqlogo() + geom_msaBar()
-    }
-  )
+  og <- isolate(seq_dataInput())
+  if (length(og) != 1) {
+    output$msa_plot <- renderPlot({ plot.new(); text(0.5, 0.5, "Please specify a single ortholog group.") })
+    return(NULL)
+  }
+  
+  # データベースからFASTA取得
+  query <- paste0("SELECT fasta FROM orthogroup_data WHERE og_id = '", og, "'")
+  res <- dbGetQuery(con, query)
+  
+  if (nrow(res) == 1) {
+    fasta_text <- res$fasta[1]
+    file <- tempfile(fileext = ".fa")
+    writeLines(fasta_text, file)
+    
+    mfa <- readAAStringSet(file, "fasta")
+    msa.seqs <- msa(mfa, "ClustalOmega")
+    
+    output$msa_plot <- renderPlot(
+      width = 50 + max(width(msa.seqs@unmasked)) * 22,
+      height = 100 + length(msa.seqs@unmasked) * 30,
+      res = 120,
+      {
+        ggmsa(msa.seqs@unmasked, char_width = 0.5, seq_name = TRUE) +
+          theme(axis.text = element_text(size = 12)) +
+          geom_seqlogo() + geom_msaBar()
+      }
+    )
+  } else {
+    output$msa_plot <- renderPlot({ plot.new(); text(0.5, 0.5, "FASTA not found in database.") })
+  }
 })
 
 ## get ggtree
 observeEvent(input$show_tree, {
-  if(length(seq_dataInput()) == 1){
-    output$tree_text <- renderText({return(paste("Phylogenetic tree of the ortholog group", isolate(seq_dataInput())))})
-    file <- paste0(tree_dir,"/",isolate(seq_dataInput()),"_tree.txt")
-    tree.f <- read.tree(file)
+  og <- isolate(seq_dataInput())
+  if (length(og) != 1) {
+    output$tree_text <- renderText({ "ERROR: Please specify a single ortholog group." })
+    output$tree_plot <- NULL
+    return(NULL)
+  }
+  
+  output$tree_text <- renderText({ paste("Phylogenetic tree of the ortholog group", og) })
+  
+  query <- paste0("SELECT tree FROM orthogroup_data WHERE og_id = '", og, "'")
+  res <- dbGetQuery(con, query)
+  
+  if (nrow(res) == 1) {
+    tree_str <- res$tree[1]
+    tree_file <- tempfile(fileext = ".nwk")
+    writeLines(tree_str, tree_file)
+    tree.f <- read.tree(tree_file)
+    
     output$tree_plot <- renderPlot(
-      width=600,
-      height=600,
-      res=100,
+      width = 600,
+      height = 600,
+      res = 100,
       {
-      ggtree(tree.f) +  geom_tiplab() + xlim(0, 2)
-      ## https://support.bioconductor.org/p/72398/
+        ggtree(tree.f) + geom_tiplab() + xlim(0, 2)
       }
     )
-  }
-  else{
-    output$tree_text <- renderText({return("ERROR: Please specify a single ortholog group.")})
+  } else {
+    output$tree_text <- renderText({ "Tree not found in database." })
     output$tree_plot <- NULL
   }
 })
